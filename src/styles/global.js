8a16b4172a138f7feda791a14a2aed15eac3e394
Robson Thomaz Castelli <robson@webstationdeisgn.com.br>
2020-03-06 15:20:40 -0300

    última versão valida

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,22 +1,11 @@
 import { createGlobalStyle } from 'styled-components';
 
-import 'react-perfect-scrollbar/dist/css/styles.css';
-import 'react-toastify/dist/ReactToastify.css';
-
-import bg from '~/assets/bg.jpg';
-
 export default createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
-
   * {
     margin: 0;
     padding: 0;
     outline: 0;
     box-sizing: border-box;
-
-    &:focus{
-      outline: 0;
-    }
   }
 
   html, body, #root {
@@ -24,21 +13,14 @@ export default createGlobalStyle`
   }
 
   body{
+    background: #7159c1;
     -webkit-font-smoothing: antialiased !important;
-    background: url(${bg}) center center no-repeat;
-    background-size: cover;
   }
 
   body, input, button {
-    font: 14px 'Roboto', sans-serif;
-  }
-
-  a {
-    text-decoration: none;
-  }
-
-  ul {
-    list-style: none;
+    color: #222;
+    font-size: 14px;
+    font-family: Arial, Helvetica, sans-serif;
   }
 
   button {
